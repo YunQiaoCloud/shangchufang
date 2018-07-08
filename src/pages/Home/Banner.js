7703b51f940102c66eb9095fac55f59333602654
jinzhuming <jinzhuming0308@gmail.com>
2018-07-08 15:31:23 +0800

    Banner 改为请求后端数据

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
-import { Carousel, WingBlank } from 'antd-mobile'
+import { Carousel } from 'antd-mobile'
 import axios from 'axios'
-import tabItemData0 from './tabItemData0'
 
 class Banner extends Component {
+  state = {
+    banner: []
+  }
+
   async componentDidMount() {
-    const res = await axios.get('/api/banner')
-    console.log(res)
+    try {
+      const res = await axios.get('/api/banner')
+      this.setState(() => ({ banner: res.data }))
+    } catch (err) {
+      console.log('获取数据失败')
+    }
   }
 
   render() {
-    const dom = tabItemData0.slice(0, 4).map((item) => {
+    const { banner } = this.state
+    console.log(banner)
+
+    const dom = banner.map((item) => {
+      console.log(item)
       return (
         <a href="javascript:;" className="Home-banner-item" key={item.id}>
-          <img className="cover" src={item.albums[0]} alt={item.title} />
+          <img className="cover" src={item.coverImg} alt={item.title} />
           <p className="title">
             {item.title}
           </p>
